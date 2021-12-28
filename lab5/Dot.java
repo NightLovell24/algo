5aaaff59730a08a23c8ed3a7abc4d8b2260a4c1d
NightLovell24 <65086119+NightLovell24@users.noreply.github.com>
2021-12-28 16:27:29 +0200

    Add files via upload

---
+++ lab5/Dot.java
@@ -0,0 +1,19 @@
+package my.nalab5;
+
+public class Dot {
+
+	int x;
+	int y;
+
+	public Dot() {
+		x = -1;
+		y = -1;
+	}
+
+	public Dot(int x, int y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+
+}
---
+++ lab5/Player.java
@@ -0,0 +1,18 @@
+package my.nalab5;
+
+public class Player {
+
+	private int score;
+	
+
+	
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+}
